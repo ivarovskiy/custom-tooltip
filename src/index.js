b3e019250dc638bba40d5784637729385ba671f6
Ivashchenko <aivashchenko@luxoft.com>
2022-01-25 18:58:18 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-import './my-element.js';
+import './ls-try.js';
--- src/my-element.js
+++ src/my-element.js
@@ -1,13 +1,88 @@
-import {html, LitElement, property} from 'lit-element';
+import { LitElement, html, css } from "lit";
 
-class MyElement extends LitElement {
-  @property({type: String}) myProp = 'stuff';
+class CustomInspector extends LitElement {
+  constructor() {
+    super();
+
+    this.tagName = '';
+    this.element = document;
+    this.element.addEventListener('mouseover', this.handlerEvent);
+    this.element.addEventListener('mouseout', this.handlerEvent);
+  }
+
+  
+  static get properties() {
+    return {
+      tagName: { type: String },
+    }
+  }
+
+  static get styles() {
+    return css`
+      .info {
+        position: absolute;
+        font-size: 200px;
+        width: max-content;
+        display: inline-block;
+        border: 1px solid black;
+        padding: 10px;
+        background: white;
+        transition: 0.6s all;
+        opacity: 1;
+        position: absolute;
+        bottom: 20px;
+        left: 10px;
+        z-index: 3;
+        color: black;
+      }
+    `
+  }
+
+  // update(changedProperties) {
+  //   changedProperties.forEach((oldValue, propName) => {
+  //     console.log(`${propName} changed. oldValue: ${oldValue}`);
+  //   });
+  // }
+
+  handlerEvent(event) {
+    function str(el) {
+      if (!el) {
+        return "null"
+      }
+
+      if ((el.tagName).toLowerCase() !== 'body' && (el.tagName).toLowerCase() !== 'html') {
+        return el.className || el.tagName;
+      }
+    }
+
+    this.tagName = str(event.target);
+
+    if (this.tagName !== undefined) {
+      if (event.type === 'mouseover') {
+        event.target.style.background = 'rgb(170,200,250,0.3)';
+        event.target.style.outline = '1px solid blue';
+        event.target.title = this.tagName;
+        console.log(event.clientX + ' ' + event.clientY)
+      }
+
+      if (event.type === 'mouseout') {
+        event.target.style.background = '';
+        event.target.style.outline = 'none';
+      }
+    }
+  }
+
+  // positionOfCoursor(event) {
+
+  // }
+
   render() {
     return html`
-      <p>Hello World</p>
-      ${this.myProp}
-    `;
+      <div class="info">
+        ${this.tagName}
+           Tag name is
+      </div>`;
   }
 }
 
-customElements.define('my-element', MyElement);
+customElements.define('custom-inspector', CustomInspector);
